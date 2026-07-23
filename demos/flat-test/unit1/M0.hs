8f9ba092c477c619ab032fb4cb339df9fea1b1bc
Andrea Vezzosi <andrea@well-typed.com>
2026-07-23 17:14:52 +0200

    more interesting flat-test

--- demos/flat-test/unit1/M0.hs
+++ demos/flat-test/unit1/M0.hs
@@ -1,17 +1,29 @@
+{-# LANGUAGE BangPatterns #-}
 module M0 where
 
 import qualified M1
-import Data.Map
 
+main = do
+  print $ take 100 $ runLengths "aaabbc"
+
+-- Group consecutive equal elements and count them,
+-- e.g. runLengths "aaabbc" = [('a',3),('b',2),('c',1)]
+runLengths :: Eq a => [a] -> [(a, Int)]
+runLengths []     = []
+runLengths (x:xs) = go x 1 xs
+  where
+    go c !n []                 = [(c, n)]
+    go c !n (y:ys)
+      | y == c    = go c (n + 1) ys
+      | otherwise = (c, n) : go y 1 xs
+
+
+
+
+
+-- tests M1 dependency
 chunk0 :: Int
 chunk0 = M1.value
 
 m0_value :: Int
 m0_value = chunk0
-
-
-main = do
-  pure ()
-  pure ()
-  arg <- pure chunk0
-  print arg
--- demos/flat-test/unit1/M1.hs
+++ demos/flat-test/unit1/M1.hs
@@ -1,6 +1,7 @@
 module M1 where
 
+-- tests dependencies on external packages.
+import Data.Map
 
 value :: Int
 value = 1
-
